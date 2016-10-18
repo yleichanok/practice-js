65a210e80aa080086cb9a9a3999fe017bdd11fdf
Evgeny Leychenok <e.leychenok@gmail.com>
2016-10-18 16:15:56 -0700

    [Linked list] Added remove method

--- data-structures/linked-list/singly.js
+++ data-structures/linked-list/singly.js
@@ -55,16 +55,6 @@ SinglyLinkedList.prototype.prepend = function(data) {
 };
 
 /**
- * Inserts a new node after some existing node.
- * @param  {Node} node    Existing node
- * @param  {Node} newNode New node to insert
- * @todo
- */
-SinglyLinkedList.prototype.insertAfter = function(node, newNode) {
-
-};
-
-/**
  * Reverses the order of the list elements.
  */
 SinglyLinkedList.prototype.reverse = function() {
@@ -90,10 +80,27 @@ SinglyLinkedList.prototype.reverse = function() {
 /**
  * Removes one node from the list; updates links.
  * @param  {Node} node Node to remove
- * @todo
  */
 SinglyLinkedList.prototype.remove = function(node) {
 
+    var prevNode = null,
+        curNode = this.head;
+
+    while (curNode !== node) {
+        prevNode = curNode;
+        curNode = curNode.next;
+    }
+
+    // update reference to the next node
+    prevNode.next = node.next;
+
+    // remove node from the storage
+    for (var i = 0; i < this._els.length; i++) {
+        if (this._els[i] === node) {
+            this._els.splice(i, 1);
+            break;
+        }
+    }
 };
 
 /**
