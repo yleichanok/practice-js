3c04257eb5f49c17990ef25ac532a3bd30be33bb
Evgeny Leychenok <e.leychenok@gmail.com>
2016-10-17 20:26:43 -0700

    Stack and queue implementation

---
+++ data-structures/queue/queue.js
@@ -0,0 +1,86 @@
+/**
+ * Queue implementation.
+ * @param {Number} maxSize Maximum number of elements in the queue (optional).
+ * @see https://en.wikipedia.org/wiki/Queue_(abstract_data_type)
+ */
+function Queue(maxSize) {
+
+    /**
+     * Maximum number of elements in the queue.
+     * @optional
+     * @private
+     * @type {Number}
+     */
+    this._maxSize = maxSize;
+
+    /**
+     * Pointer to the last element of the queue.
+     * @private
+     * @type {Number}
+     */
+    this._last = -1;
+
+    /**
+     * Elements of the queue.
+     * @private
+     * @type {Array}
+     */
+    this._els = [];
+}
+
+/**
+ * Adds element to the queue.
+ * If the queue is full - throws an error.
+ * @param  {any} el New element
+ */
+Queue.prototype.enqueue = function(el) {
+
+    if (this.isFull()) {
+        throw new Error('Reached maximum size of the queue.');
+    }
+
+    this._els[++this._last] = el;
+};
+
+/**
+ * Removes element from the queue.
+ * If the queue is empty - throws an error.
+ * @return {any} Element
+ */
+Queue.prototype.dequeue = function() {
+
+    if (this.isEmpty()) {
+        throw new Error('Queue is empty.');
+    }
+
+    var el = this._els[0];
+    this._els.splice(0, 1);
+    this._last--;
+    return el;
+};
+
+/**
+ * Returns the next element of the queue without removing it.
+ * If the queue is empty - returns null.
+ * @return {any} Element
+ */
+Queue.prototype.peek = function() {
+    return this._els[0] || null;
+};
+
+/**
+ * Checks if the queue does not contain any elements.
+ * @return {Boolean}
+ */
+Queue.prototype.isEmpty = function() {
+    return this._last === -1;
+};
+
+/**
+ * Checks if the queue is full.
+ * If maxSize is not set - always returns false.
+ * @return {Boolean}
+ */
+Queue.prototype.isFull = function() {
+    return this._maxSize && this._last >= this._maxSize - 1;
+};
---
+++ data-structures/stack/stack.js
@@ -0,0 +1,85 @@
+/**
+ * Implementation of stack.
+ * @param {Number} maxSize Maximum size of the stack (optional).
+ * @see https://en.wikipedia.org/wiki/Stack_(abstract_data_type)
+ */
+function Stack(maxSize) {
+
+    /**
+     * Maximum size of the stack.
+     * @optional
+     * @private
+     * @type {Number}
+     */
+    this._maxSize = maxSize;
+
+    /**
+     * Pointer to the top element of the stack.
+     * @private
+     * @type {Number}
+     */
+    this._top = -1;
+
+    /**
+     * Stack contents.
+     * @private
+     * @type {Array}
+     */
+    this._els = [];
+}
+
+/**
+ * Adds the element on top pf the stack.
+ * If the stack is full - throws an error.
+ * @param  {any} el New element.
+ */
+Stack.prototype.push = function(el) {
+
+    if (this.isFull()) {
+        throw new Error('Reached maximum size of the stack.');
+    }
+
+    this._els[++this._top] = el;
+};
+
+/**
+ * Removes the top element from the stack and returns it.
+ * If the stack is empty - throws an error.
+ * @return {any} Top element of the stack.
+ */
+Stack.prototype.pop = function() {
+
+    if (this._top === -1) {
+        throw new Error('Stack is empty.');
+    }
+
+    var el = this._els[this._top];
+    this._els.splice(this._top--, 1);
+    return el;
+};
+
+/**
+ * Returns the top element of the stack without removing it.
+ * If the stack is empty - returns null.
+ * @return {any} Top element of the stack.
+ */
+Stack.prototype.peek = function() {
+    return this._els[this._top] || null;
+};
+
+/**
+ * Checks if the stack is empty - ie. contains no elements.
+ * @return {Boolean}
+ */
+Stack.prototype.isEmpty = function() {
+    return this._top === -1;
+};
+
+/**
+ * Checks if the stack is full. 
+ * If maxSize was not set - always returns false.
+ * @return {Boolean}
+ */
+Stack.prototype.isFull = function() {
+    return this._maxSize && this._top >= this._maxSize - 1;
+};
