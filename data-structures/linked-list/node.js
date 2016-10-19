4c7834c3e70427e60a16592bd033fd6279909d48
Evgeny Leychenok <e.leychenok@gmail.com>
2016-10-19 15:58:13 -0700

    [Linked list] XOR linked list - basic functions

--- data-structures/linked-list/node.js
+++ data-structures/linked-list/node.js
@@ -9,3 +9,21 @@ function Node(data, next, prev) {
     this.next = next || null;
     this.prev = prev || null;
 }
+
+function XORNode(data, id, nextId, prevId) {
+    this.data = data;
+    this.id = id;
+
+    this._ref = (prevId || 0) ^ (nextId || 0);
+}
+
+XORNode.prototype.getNextId = function(prevId) {
+    return (prevId || 0) ^ this._ref;
+};
+XORNode.prototype.getPrevId = function(nextId) {
+    return (nextId || 0) ^ this._ref;
+};
+
+XORNode.prototype.updateRef = function(nextId, prevId) {
+    this._ref = (prevId || 0) ^ (nextId || 0);
+};
