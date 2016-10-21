f84b332443105cdf167c89a82d294a1e47df8895
Evgeny Leychenok <e.leychenok@gmail.com>
2016-10-21 14:06:11 -0700

    Brackets balance checker

---
+++ algorithms/brackets-balance/isBalanced.js
@@ -0,0 +1,50 @@
+/**
+ * Checks if brackets in the input text are balanced.
+ *
+ * If the bracket is opening - push it to the stack;
+ * if the bracket is closing - check the value in the stack:
+ *     if it is a corresponding opening bracket - remove it from the stack.
+ * Stack has to end up empty.
+ * 
+ * @param  {String} text Text to check
+ * @return {Boolean}
+ */
+function isBalanced(text) {
+
+    if (!text && text !== '') {
+        throw new Error('Text is missing.');
+    }
+
+    var pairs = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    var stack = new Stack(text.length);
+
+    for (var i = 0, l = text.length; i < l; i++) {
+        var char = text[i];
+        switch(char) {
+        case '(':
+        case '{':
+        case '[':
+            stack.push(char);
+            break;
+        case ')':
+        case '}':
+        case ']':
+            if (stack.isEmpty()) {
+                return false;
+            }
+
+            var previous = stack.pop();
+            if (!previous || previous !== pairs[char]) {
+                return false;
+            }
+            break;
+        }
+    }
+
+    return stack.isEmpty();
+}
