c47aaef4968f6d1b36482be5430a05973ba9410e
Evgeny Leychenok <e.leychenok@gmail.com>
2016-10-23 14:27:21 -0700

    Prefix notation parsing

---
+++ algorithms/expression-parse/prefix/evaluate.js
@@ -0,0 +1,64 @@
+/**
+ * Evaluates a mathematical expression written in prefix ("polish") notation.
+ * Operators and operands must be separated with whitespaces.
+ * @see https://en.wikipedia.org/wiki/Polish_notation
+ * 
+ * @param  {String} expr Expression to evaluate
+ * @return {Number}
+ */
+function evaluate(expr) {
+
+    var regex = new RegExp('[0-9 +-/\*]', 'g');
+
+    // test input to contain only digits and basic mathematical operations
+    if (typeof expr !== 'string' || !regex.test(expr)) {
+        throw new Error('Invalid input.');
+    }
+
+    var s = new Stack(),
+        tokens = expr.split(/\s+/g).reverse(),
+        token;
+
+    while (token = tokens.shift()) {
+
+        var num = parseFloat(token);
+
+        // if the token is a number - push it to the stack
+        // if it's an operation - pop the last to elements from the stack
+        // and perform the operation
+        if (!isNaN(num)) {
+            s.push(num);
+        } else {
+            if (s.size() < 2) {
+                throw new Error('Invalid input.');
+            }
+
+            var op1 = s.pop(),
+                op2 = s.pop(),
+                res;
+
+            switch (token) {
+            case '+':
+                s.push(op1 + op2);
+                break;
+            case '-':
+                s.push(op1 - op2);
+                break;
+            case '*':
+                s.push(op1 * op2);
+                break;
+            case '/':
+                s.push(op1 / op2);
+                break;
+            default:
+                throw new Error('Unknown operation: ' + token);
+            }
+        }
+    }
+
+    if (s.size() > 1) {
+        throw new Error('Invalid input.');
+    }
+
+    return s.pop();
+}
