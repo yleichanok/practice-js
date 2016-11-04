e91961775308c72c932eab9d42515a74924dd2dc
Evgeny Leychenok <e.leychenok@gmail.com>
2016-11-04 10:36:07 -0700

    [Sort] Merge sort - better implementation

--- algorithms/sort/merge/sort.js
+++ algorithms/sort/merge/sort.js
@@ -4,7 +4,7 @@
  *
  * Average time complexity: O(n log n)
  */
-Array.prototype.mergeSort = function() {
+Array.prototype.mergeSort1 = function() {
     
     // exit if nothing to sort
     if (this.length <= 1) {
@@ -16,8 +16,8 @@ Array.prototype.mergeSort = function() {
         arr2 = this.slice(pivot + 1);
 
     // split initial array into small subarrays
-    arr1.mergeSort();
-    arr2.mergeSort();
+    arr1.mergeSort1();
+    arr2.mergeSort1();
 
     var i = 0;
 
@@ -34,4 +34,56 @@ Array.prototype.mergeSort = function() {
 
         i++;
     }
+};
+
+/**
+ * Sorts an array using merge sort.
+ * This implementation is index-based and uses only one temporary array.
+ */
+Array.prototype.mergeSort2 = function() {
+
+    // exit if nothing to sort
+    if (this.length <= 1) {
+        return;
+    }
+
+    var self = this,
+        arr = new Array(this.length);
+
+    function merge(start, middle, end) {
+
+        var i = start, j = middle + 1;
+
+        for (var k = start; k <= end; k++) {
+            if (i <= middle && (j > end || self[i] < self[j])) {
+                arr[k] = self[i];
+                i++;
+            } else {
+                arr[k] = self[j];
+                j++;
+            }
+        }
+    }
+
+    function split(start, end) {
+
+        if (start >= end) {
+            return;
+        }
+
+        // split array into smaller subarrays
+        var middle = Math.floor((end + start) / 2);
+        split(start, middle);
+        split(middle + 1, end);
+
+        // then merge them in order
+        merge(start, middle, end);
+
+        // copy from temp array to the initial
+        for (var i = start; i <= end; i++) {
+            self[i] = arr[i];
+        }
+    }
+
+    split(0, this.length - 1);
 };
