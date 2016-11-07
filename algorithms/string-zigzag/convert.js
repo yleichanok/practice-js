ab97f9394ca7144c7ad9784f479d5d3c3e0c31c4
Evgeny Leychenok <e.leychenok@gmail.com>
2016-11-06 20:17:38 -0800

    [String zigzag] Better implementation

--- algorithms/string-zigzag/convert.js
+++ algorithms/string-zigzag/convert.js
@@ -14,7 +14,7 @@
  * @param  {Number} rows Number of rows to use
  * @return {String}      Result string
  */
-function convert(s, rows) {
+function convert1(s, rows) {
 
     if (typeof s !== 'string') {
         throw new Error('Invalid input.');
@@ -27,7 +27,7 @@ function convert(s, rows) {
         return s;
     }
 
-    // create array of N arrays, where N == rows
+    // create an array of N arrays, where N == rows
     // to store letters in each row
     var res = Array.from({length: rows}, () => []),
         increment = 1,
@@ -37,15 +37,74 @@ function convert(s, rows) {
 
         res[row].push(s[i]);
 
+        // check if we need to go backwards
         if (row === rows - 1 && increment > 0) {
             increment = -1;
         } else if (row === 0 && increment < 0) {
             increment = 1;
         }
 
+        // set next row index
         row += increment;
     }
 
     // merge arrays into one
     return [].concat.apply([], res).join('');
+}
+
+/**
+ * Probably a better implementation. Uses only one array for the result string.
+ * 
+ * @param  {String} s    Initial string
+ * @param  {Number} rows Number of rows
+ * @return {String}      Result string
+ */
+function convert2(s, rows) {
+
+    if (typeof s !== 'string') {
+        throw new Error('Invalid input.');
+    }
+    if (rows <= 0) {
+        throw new Error('Invalid number of rows.');
+    }
+
+    if (s.length < 3) {
+        return s;
+    }
+
+    var res = new Array(s.length),
+        step = 2 * rows - 2;
+
+    for (var i = 0; i < rows; i++) {
+
+        if (i === 0 || i === rows - 1) {
+
+            // every Nth letter goes in the first or last row
+            // where N == step
+            for (var j = i; j < s.length; j += step) {
+                res.push(s[j]);
+            }
+        } else {
+
+            // magic formula for to find a step for letters in other rows;
+            // step is alternating
+            var step1 = 2 * (rows - 1 - i),
+                step2 = step - step1,
+                back = false;
+
+            for (var j = i; j < s.length;) {
+                res.push(s[j]);
+
+                if (back) {
+                    j += step2;
+                } else {
+                    j += step1;
+                }
+
+                back = !back;
+            }
+        }
+    }
+
+    return res.join('');
 }
