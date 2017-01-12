efb5691633086ef95b17d5775b135e997fdb6182
Evgeny Leychenok <e.leychenok@gmail.com>
2017-01-11 19:27:34 -0800

    [Power-two] Different implementation

--- algorithms/power-two/check.js
+++ algorithms/power-two/check.js
@@ -11,3 +11,21 @@ function check1(num) {
 
     return num !== 0 && (num & (num - 1)) === 0;
 }
+
+/**
+ * Different implementation.
+ * @param  {Number} num Input number
+ * @return {Boolean}
+ */
+function check2(num) {
+
+    if (typeof num !== 'number') {
+        throw new Error('Invalid input.');
+    }
+
+    while (num > 1) {
+        num /= 2;
+    }
+
+    return num === 1;
+}
